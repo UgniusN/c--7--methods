e5c571d77187b0c4aea970577738bee54cd21dad
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-16 14:07:22 +0200

    Finished App9

--- App9/App9/Program.cs
+++ App9/App9/Program.cs
@@ -10,6 +10,25 @@ namespace App9
     {
         static void Main(string[] args)
         {
+            printLine(20);
+        }
+
+
+        public static void printLine(int a)
+        {
+            char[] raides = new char[a];
+            int max;
+            String text;
+            string simboliai = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            max = simboliai.Length;
+            Random rnd = new Random();
+            for (int i=0; i<a; i++)
+            {
+                raides[i] = simboliai[rnd.Next(0, max)];
+            }
+            text = new string(raides);
+            Console.WriteLine(text);
+            Console.ReadLine();
         }
     }
 }
