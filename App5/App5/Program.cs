514b70c37541b29e5306cf5bec49687af7ff0837
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-16 12:36:14 +0200

    Completed App5

--- App5/App5/Program.cs
+++ App5/App5/Program.cs
@@ -10,6 +10,27 @@ namespace App5
     {
         static void Main(string[] args)
         {
+            uzdetiRemeli("Trying the text that should fit into the frames.");
+        }
+
+        public static void uzdetiRemeli(string a)
+        {
+            int lettercount = 0;
+            foreach (char letter in a)
+            {
+                lettercount++;
+            }
+            for (int i = 0; i < lettercount + 4; i++)
+            {
+                Console.Write("♦");
+            }
+            Console.WriteLine();
+            Console.WriteLine("♦ {0} ♦", a);
+            for (int i = 0; i < lettercount + 4; i++)
+            {
+                Console.Write("♦");
+            }
+            Console.Read();
         }
     }
 }
