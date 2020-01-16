2bc917fa91bda4cc1eab44d7fdf76ea7904871ea
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-16 12:44:14 +0200

    Completed App6

--- App6/App6/Program.cs
+++ App6/App6/Program.cs
@@ -10,6 +10,24 @@ namespace App6
     {
         static void Main(string[] args)
         {
+            int[] masyvas = { 1, 5, 4, 5, 6, 7, 8 };
+            masyvoIsvedimas(masyvas);
+
+        }
+
+
+        public static void masyvoIsvedimas(int [] arr)
+        {
+            for(int i=0; i<arr.Length; i++)
+            {
+                if(i == arr.Length-1)
+                {
+                    Console.Write(arr[i]);
+                }
+                else
+                Console.Write("{0},", arr[i]);
+            }
+            Console.Read();
         }
     }
 }
