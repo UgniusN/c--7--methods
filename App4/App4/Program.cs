71612db2e2f6c67ddf582c090595103e0f7c2b40
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-16 12:21:52 +0200

    Completed App4

---
+++ App4/App4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(stringCount("Labas as esu ugnius"));
+            Console.Read();
+        }
+
+        public static String stringCount(String a)
+        {
+            int count = 0;
+            foreach(char letter in a)
+            {
+                count++;
+            }
+            String countString = count.ToString();
+            return countString;
+        }
+    }
+}
