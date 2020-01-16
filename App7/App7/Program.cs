6ca9582cedac0ed90fa232d3a069ecea443096f8
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-16 13:12:09 +0200

    Completed App7

--- App7/App7/Program.cs
+++ App7/App7/Program.cs
@@ -10,6 +10,20 @@ namespace App7
     {
         static void Main(string[] args)
         {
+            decimal[] skaiciai = { 3.5575474m,3.5432537m,5.4325322m,4.677432m};
+            Console.WriteLine(apvalinimasIrSumavimas(skaiciai));
+            Console.Read();
+        }
+
+
+        public static decimal apvalinimasIrSumavimas(decimal [] a)
+        {
+            decimal suma = 0 ;
+            foreach(decimal skaicius in a)
+            {
+                suma += skaicius;
+            }
+            return Math.Round(suma, 2);
         }
     }
 }
