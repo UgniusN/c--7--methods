ba7f56f821a54156373443daec43a7f5a2f327be
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 10:22:16 +0200

    Completed App10

--- App10/App10/Program.cs
+++ App10/App10/Program.cs
@@ -10,6 +10,32 @@ namespace App10
     {
         static void Main(string[] args)
         {
+            string zodis = "Grazus vakaras";
+            string result = "";
+            string result2 = "";
+            for (int i = 0; i < zodis.Length - 1; i++)
+            {
+                int simbolis = (int)zodis[i];
+                simbolis += 2;
+                result = result + (char)simbolis;
+            }
+
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                int simbolis = (int)result[i];
+                simbolis -= 2;
+                result2 = result2 + (char)simbolis;
+            }
+
+            Console.WriteLine("Pradine fraze: {0}, sifruota fraze: {1}, po dekodavimo: {2}", zodis, result, result2);
+
+            //char c = 'a';
+            //int i = (int)c;
+            //Console.WriteLine("c: {0}, (int)c:{1}", c, i);
+            //i++;
+            //c = (char)i;
+            //Console.WriteLine("c: {0}, (int)c:{1}", c, i);
+            Console.ReadLine();
         }
     }
 }
